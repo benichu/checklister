4056f16e6e78d56ba9ef1ed11020ff8a99c70c36
Manon Deloupy <manon.deloupy@intello.com>
2015-08-18 08:17:42 -0400

    Require Coveralls before requiring the app, according to doc

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'checklister'
 require 'coveralls'
 Coveralls.wear!
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'checklister'
