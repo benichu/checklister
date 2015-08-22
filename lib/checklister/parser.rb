57fa162bf0363650e1afad0a85bfa65c007275c2
Manon Deloupy <manon.deloupy@intello.com>
2015-08-21 20:03:05 -0400

    Add comments and init specs

--- lib/checklister/parser.rb
+++ lib/checklister/parser.rb
@@ -1,4 +1,39 @@
+# Parse a markdown file to return an exploitable ruby object :
+#   {
+#     title: "Title",
+#     body: "## Mardown Body context\n## Checklist\nMardown Body checklist"
+#   }
+#
 module Checklister
   class Parser
+
+    # Params :
+    # - file : the path of the markdown file to parse
+    def initialize(file)
+    end
+
+    # What should the issue title be ?
+    # The headline of the markdown document, minus any markdown-related caracter(# or =)
+    # Returns a string
+    def parse_title
+    end
+
+    # What should the issue body be ?
+    # Context (if any) + actual checklist, without the subtitles
+    # Returns a string
+    def parse_body
+    end
+
+    # Returns a ruby object, example :
+    # {
+    #   title: "Title",
+    #   body: "## Mardown Body context\n## Checklist\nMardown Body checklist"
+    # }
+    def issue_params
+      {
+        title: parse_title,
+        body: parse_body
+      }
+    end
   end
 end
--- spec/checklister/parser_spec.rb
+++ spec/checklister/parser_spec.rb
@@ -1,4 +1,17 @@
 require "spec_helper"
 
 describe Checklister::Parser do
+  describe "#parse_title" do
+    it "should return a parsed title"
+  end
+
+  describe "#parse_body" do
+    it "should return a parsed body"
+  end
+
+  describe "#issue_params" do
+    it "should correctly assign the title"
+    it "should correctly assign the body"
+    it "should return a ruby object"
+  end
 end
