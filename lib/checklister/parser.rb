5520be1c470993d4263274dc0ca5ae4de521ad74
Benjamin Thouret <ben@2ret.com>
2015-08-24 07:42:51 -0400

    Quick and Dirty prototype.

--- lib/checklister/parser.rb
+++ lib/checklister/parser.rb
@@ -15,9 +15,40 @@ module Checklister
 
     # @return [Hash] a hash of params
     def to_params
+      title     = ""
+      context   = []
+      building_context = false
+      checklist = []
+      building_checklist = false
+
+      @file_content.each_line do |line|
+        if title == "" && line.start_with?("# ")
+          title = line.sub("# ", "").sub("\n", "")
+        end
+
+        if line.start_with?("## Context")
+          building_context = true
+          next
+        end
+
+        if line.start_with?("## Checklist")
+          building_context = false
+          building_checklist = true
+          next
+        end
+
+        if building_context
+          context << line.sub("\n", "") unless ["", "\n"].include?(line.chomp)
+        end
+
+        if building_checklist
+          checklist << line.sub("\n", "") unless ["", "\n"].include?(line.chomp)
+        end
+      end
+
       {
-        title: parse_title,
-        body: parse_body
+        title: title,
+        body: "#{context.join("\n")}\n#{checklist.join("\n")}"
       }
     end
 
