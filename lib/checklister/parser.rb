0e2b0f137b67ef2dba7981c77d3e8ac617bdcde3
Manon Deloupy <manon.deloupy@intello.com>
2015-09-01 08:21:18 -0400

    Clean up & add specs

--- lib/checklister/parser.rb
+++ lib/checklister/parser.rb
@@ -51,55 +51,5 @@ module Checklister
         body: "#{context.join("\n")}\n#{checklist.join("\n")}"
       }
     end
-
-    private
-
-    # The title should be the plain text headline of the markdown document
-    #
-    # @return [String] the parsed title
-    def parse_title
-      @file_content.each_line do |line|
-        if line.start_with?("# ")
-          # Remove any markdown element
-          # Remove extra `\n` at the end of the string if any
-          return line.sub("# ", "").sub("\n", "")
-          break
-        end
-      end
-    end
-
-    # The body is composed of the parsed context (if any) and the checklist
-    #
-    # @return [String] the parsed body
-    def parse_body
-      "#{get_context}\n#{get_checklist}"
-    end
-
-    def get_context
-      context = ""
-      @file_content.each_line do |line|
-        if line.start_with?("## Context")
-          # Also go to next line
-          next
-        end
-        context << line
-        next
-        if line.start_with?("##")
-          # Means we're at the end of the Context section
-          break
-        end
-      end
-      context
-    end
-
-    def get_checklist
-      checklist = ""
-      @file_content.each_line do |line|
-        if line.start_with?("- [ ]")
-          checklist << line
-        end
-      end
-      checklist
-    end
   end
 end
--- spec/checklister/parser_spec.rb
+++ spec/checklister/parser_spec.rb
@@ -1,24 +1,48 @@
 require "spec_helper"
 
 describe Checklister::Parser do
-  let(:parser) { Checklister::Parser.new("./spec/fixtures/simple-checklist.md") }
+  let(:simple_checklist) { Checklister::Parser.new("./spec/fixtures/simple-checklist.md") }
+  let(:basic_checklist) { Checklister::Parser.new("./spec/fixtures/basic-checklist.md") }
 
-  before do
-    @title = "Simple Checklist"
-    @body = "When you need to accomplish stuff, follow those steps.\n- [ ] Step 1\n- [ ] Step 2\n- [ ] Step 3"
+  context "For a default checklist" do
+    before do
+      @title = "Simple Checklist"
+      @body = "When you need to accomplish stuff, follow those steps.\n- [ ] Step 1\n- [ ] Step 2\n- [ ] Step 3"
+    end
+
+    describe "#to_params" do
+      it "should parse a title" do
+        expect(simple_checklist.to_params[:title]).to eq(@title)
+      end
+
+      it "should parse a body" do
+        expect(simple_checklist.to_params[:body]).to eq(@body)
+      end
+
+      it "should return a ruby object" do
+        expect(simple_checklist.to_params).to include(title: @title, body: @body)
+      end
+    end
   end
 
-  describe "#to_params" do
-    it "should parse a title" do
-      expect(parser.to_params[:title]).to eq(@title)
+  context "For a minimal checklist" do
+    before do
+      @title = ""
+      @body = "\n- [ ] Step 1\n- [ ] Step 2\n- [ ] Step 3"
     end
 
-    it "should parse a body" do
-      expect(parser.to_params[:body]).to eq(@body)
-    end
+    describe "#to_params" do
+      it "should parse a title" do
+        expect(basic_checklist.to_params[:title]).to eq(@title)
+      end
 
-    it "should return a ruby object" do
-      expect(parser.to_params).to include(title: @title, body: @body)
+      it "should parse a body" do
+        expect(basic_checklist.to_params[:body]).to eq(@body)
+      end
+
+      it "should return a ruby object" do
+        expect(basic_checklist.to_params).to include(title: @title, body: @body)
+      end
     end
   end
 end
