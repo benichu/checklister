5e4c6ada500798ca1661c2dfeef8c9ec55085d6c
Manon DELOUPY <mdeloupy@gmail.com>
2016-10-05 21:55:58 -0400

    [enhancement] Fixes #62 - Provide link towards created issue

--- spec/checklister/gitlab/issue_spec.rb
+++ spec/checklister/gitlab/issue_spec.rb
@@ -5,23 +5,20 @@ describe Checklister::Gitlab::Issue do
     { endpoint: "https://www.gitlab.com/api", private_token: "supersecret", kind: "gitlab" }
   end
   let(:client) { Checklister::Client.new(gitlab_config).api_client }
+  let(:issue)  { Checklister::Gitlab::Issue.new(client).create(3, title: "Beatae possimus nostrum", body: "nihil reiciendis laboriosam nihil delectus alias accusantium dolor unde.") }
+
+  before(:each) do
+    stub_request(:post, /www.gitlab.com/).
+      to_return(status: 200, body: load_fixture("issue"), headers: {})
+  end
 
   describe "#create" do
-    before(:each) do
-      stub_request(:post, /www.gitlab.com/).
-        to_return(status: 200, body: load_fixture("issue"), headers: {})
-    end
-
     context "with valid attributes" do
       it "creates an issue by calling the remote service" do
-        issue = Checklister::Gitlab::Issue.new(client).create(3, title: "Beatae possimus nostrum", body: "nihil reiciendis laboriosam nihil delectus alias accusantium dolor unde.")
         expect(issue.to_hash["project_id"]).to eq(3)
         expect(issue.to_hash["title"]).to eq("Beatae possimus nostrum")
         expect(issue.to_hash["description"]).to eq("nihil reiciendis laboriosam nihil delectus alias accusantium dolor unde.")
       end
-    end
-
-    context "with invalid attributes" do
     end
   end
 end
